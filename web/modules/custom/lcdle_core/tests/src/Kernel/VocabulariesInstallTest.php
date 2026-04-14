244c467f6dddc9101493f12ffc337afdf3f2296d
jguyon <gustavI0@gustetto.net>
2026-04-14 17:10:02 +0200

    Add three taxonomy vocabularies via lcdle_core
    themes (transversal categories, editor-managed), chroniques (optional
    per-contributor series), tags_legacy (WP import quarantine). Matches
    spec §4 content model.

---
+++ web/modules/custom/lcdle_core/tests/src/Kernel/VocabulariesInstallTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\lcdle_core\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\taxonomy\Entity\Vocabulary;
+
+/**
+ * @group lcdle_core
+ */
+final class VocabulariesInstallTest extends KernelTestBase {
+
+  protected static $modules = ['system', 'user', 'taxonomy', 'lcdle_core'];
+
+  protected function setUp(): void {
+    parent::setUp();
+    $this->installConfig(['lcdle_core']);
+  }
+
+  /**
+   * @dataProvider provideVocabularyIds
+   */
+  public function testVocabularyExists(string $vid): void {
+    $vocabulary = Vocabulary::load($vid);
+    $this->assertNotNull($vocabulary, "Vocabulary {$vid} is installed.");
+  }
+
+  public static function provideVocabularyIds(): array {
+    return [
+      'themes' => ['themes'],
+      'chroniques' => ['chroniques'],
+      'tags_legacy' => ['tags_legacy'],
+    ];
+  }
+
+}
